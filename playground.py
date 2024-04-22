3380c200d824438222a46d1fb239c0bdd8d4ee50
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-04-22 13:52:44 +0100

    feat(send_message_to_queue): add function to send message to sqs
    example code for sending message to sqs, getting the queue url and posting to queue.

--- playground.py
+++ playground.py
@@ -17,9 +17,27 @@ def upload_to_bucket(
     :param key: The name of the key to upload to
     :return: AWS response
     """
-    client = boto3.client("s3")
+    client = boto3.client("s3", region_name="eu-west-2")
     example = {"Johnny": contents}
     file_obj = io.BytesIO()
     file_obj.write(json.dumps(example).encode("utf-8"))
     file_obj.seek(0)
     return client.put_object(Body=file_obj, Bucket=bucket, Key=key)
+
+
+def send_message_to_queue(
+    contents: str,
+    queue_name: str,
+) -> dict:
+    """
+    Send message to simple queue service specified.
+
+    :param contents: The contents of the message onto the queue
+    :param queue_name: The URL of the Amazon SQS queue to which a message is sent.
+    """
+    client = boto3.client("sqs", region_name="eu-west-2")
+    queue_url = client.get_queue_url(QueueName=queue_name)["QueueUrl"]
+    message_body = {
+        "message": contents,
+    }
+    return client.send_message(QueueUrl=queue_url, MessageBody=json.dumps(message_body))
