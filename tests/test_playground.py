ffa81b81c7013dc960aadbf8deaedaa484675d6f
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-04-22 13:55:17 +0100

    test(playground): add new test `test_playground_sqs` and create sqs for test
    `setup_data` updated to be scoped as `module`, rather than `function` updated docstring providing different scopes, also create new sqs for test. `test_playground_sqs` added to assert message sent successfully to localstack queue created.

--- tests/test_playground.py
+++ tests/test_playground.py
@@ -33,23 +33,41 @@ def setup(request):
     request.addfinalizer(remove_container)
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def setup_data():
     """
-    A function scoped fixture, which will be executed before running every test.
-    Create a new s3 bucket ready to be used.
+    Function scoped ``module``, destroyed during teardown of the last test in the module (test file).
+    Create a new s3 bucket and sqs ready for tests. **Note** the differences between pytest scope(s) because resources,
+    may already exist during test runs.
+
+    ``function``: the default scope, the fixture is destroyed at the end of the test. \n
+    ``class``: the fixture is destroyed during teardown of the last test in the class. \n
+    ``module``: the fixture is destroyed during teardown of the last test in the module. \n
+    ``package``: the fixture is destroyed during teardown of the last test in the package. \n
+    ``session``: the fixture is destroyed at the end of the test session. \n
+    https://docs.pytest.org/en/7.1.x/how-to/fixtures.html#fixture-scopes
     """
-    client = boto3.client("s3")
-    client.create_bucket(
+    s3_client = boto3.client("s3")
+    sqs_client = boto3.client("sqs")
+    s3_client.create_bucket(
         Bucket="test-bucket",
         CreateBucketConfiguration={"LocationConstraint": region_name},
     )
+    sqs_client.create_queue(QueueName="test-queue")
 
 
-def test_playground():
+def test_playground_bucket():
     result = playground.upload_to_bucket(
         contents="Europe? I'm not gonna take over Europe today.",
         bucket="test-bucket",
         key="test",
+    )
+    assert result["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+
+def test_playground_sqs():
+    result = playground.send_message_to_queue(
+        contents="Europe? I'm not gonna take over Europe today.",
+        queue_name="test-queue",
     )
     assert result["ResponseMetadata"]["HTTPStatusCode"] == 200
